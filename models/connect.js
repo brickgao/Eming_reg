48ab318419e75719dd0acacb96861d8acc33bc20
Brickgao <brickgao@gmail.com>
2013-11-11 01:27:48 +0800

    fix cookie get

--- models/connect.js
+++ models/connect.js
@@ -31,9 +31,18 @@ connect.prototype.login = function login(callback) {
     }
   };
   var req = http.request(options, function(res) {
+    var result = '',
+        resData = '',
+        headers = res.headers,
+        cookies = headers['set-cookie'];
     res.setEncoding('utf8');
-    var headers = res.headers;
+    req.write(contents);
+    cookies.forEach(function(cookie) {
+      result += cookie.replace(/path=\//g,'');
+    });
     res.on('data', function(data) {
+    });
+    res.on("end", function() {
     });
   });
   req.write(contents);
@@ -41,19 +50,33 @@ connect.prototype.login = function login(callback) {
 }
 
 connect.prototype.getinfo = function getinfo(callback) {
-    var http = require('http');
-    var options = {
-      host: 'my.whu.edu.cn',
-      path: '/index.portal',
-      method: 'get'
-    };
-    var req = http.get(options, function(res) {
-      var pageData = "";
-      res.setEncoding('UTF-8');
-      res.on('data', function(chunk) {
-        pageData += chunk;
-      });
+  var http = require('http');
+  var options = {
+    host: 'my.whu.edu.cn',
+    port: 80,
+    path: '/index.portal',
+    method: 'get',
+    headers: {
+      'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/\*;q=0.8',
+      'Accept-Encoding': 'gzip,deflate,sdch',
+      'Accept-Language': 'zh-CN,zh;q=0.8',
+      'Connection': 'keep-alive',
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Host': 'my.whu.edu.cn',
+      'Origin': 'http://my.whu.edu.cn',
+      'Referer': 'http://my.whu.edu.cn/',
+      'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/30.0.1599.101 Safari/537.36'
+    }
+  };
+  var req = http.get(options, function(res) {
+    var pageData = '';
+    res.setEncoding('UTF-8');
+    res.on('data', function(chunk) {
+      pageData += chunk;
     });
+    res.on('end', function() {
+    });
+  });
 }
 
 
